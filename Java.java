cdc9e9d945fe07efab962bf2d0f88c53b5e969aa
Bohdan Horokh <horokh.bohdan@gmail.com>
2017-11-03 00:00:54 +0200

    Just before reset

--- Java.java
+++ Java.java
@@ -2,4 +2,12 @@ public class Java{
 	public void doSmth() {
 		System.out.println("Hello to git commands");
 	}
+	
+	public void doSmth() {
+		System.out.println("Second commit boforem reset mixed");
+	}
+	
+	//public void doSmth() {
+//		System.out.println("Last commit boforem reset mixed");
+	}
 }
